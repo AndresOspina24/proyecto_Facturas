d22cae1c39e9cb6125286e11f3be880e333c5d91
Mechas <andrew2004.david2004@gmail.com>
2025-03-08 07:41:05 -0500

    Primer Modelo

--- Modelo/Producto.php
+++ Modelo/Producto.php
@@ -1,4 +1,49 @@
 <?php
+    class Producto{
+        string $codigo;
+        string $nombre;
+        int $stock;
+        float $valorUnitario;
 
+        function __construct($codigo, $nombre, $stock, $valorUnitario){
+            $this->codigo = $codigo;
+            $this->nombre = $nombre;
+            $this->stock = $stock;
+            $this->valorUnitario = $valorUnitario;
 
+        }
+
+        function setCodigo($codigo){
+            $this->codigo = $codigo;
+        }
+
+        function getCodigo(){
+            return $this->codigo;
+        }
+
+        function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        function getNombre(){
+        return $this->nombre;
+        }
+
+        function setStock($stock){
+            $this->stock = $stock;
+        }
+
+        function getStock(){
+            return $this->stock;
+        }
+
+        function setValorUnitario($valorUnitario){
+            $this->valorUnitario = $valorUnitario;
+        }
+
+        function getCodigo(){
+            return $this->valorUnitario;
+        }
+        
+    }
 ?>
