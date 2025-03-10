490554aa4ad14da84dca79f4b7216e7556f42755
Mechas <andrew2004.david2004@gmail.com>
2025-03-10 10:57:01 -0500

    Demás  Modelos

---
+++ Modelo/Empresa.php
@@ -0,0 +1,14 @@
+class Empresa {
+    private string $codigo;
+    private string $nombre;
+
+    public function __construct($codigo, $nombre) {
+        $this->codigo = $codigo;
+        $this->nombre = $nombre;
+    }
+
+    public function getCodigo(): string { return $this->codigo; }
+    public function getNombre(): string { return $this->nombre; }
+    public function setCodigo($codigo): void { $this->codigo = $codigo; }
+    public function setNombre($nombre): void { $this->nombre = $nombre; }
+}
